244a605a987f119912fac8b170233d798feeb3f3
jiajie chen <ga-kit@hotmail.com>
2021-05-20 10:22:55 -0700

    add enemy combatants

--- Unsolved/game.js
+++ Unsolved/game.js
@@ -4,23 +4,22 @@ var playerAttack = 10;
 var playerMoney = 10;
 
 //You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
+// console.log(playerName, playerAttack, playerHealth);
 
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 10;
 
-console.log(enemyNames[0]);
-console.log(enemyNames[1]);
-console.log(enemyNames[2]);
+// console.log(enemyNames[0]);
+// console.log(enemyNames[1]);
+// console.log(enemyNames[2]);
 
-for (var i = 0; i < enemyNames.length; i++) {
-    console.log(enemyNames[i]);
-    console.log(i);
-    console.log(enemyNames[i] + " is at " + i + " index");
-}
+// for (var i = 0; i < enemyNames.length; i++) {
+//     fight(enemyNames[i]);
 
-var fight = function() {
+// }
+
+var fight = function(enemyName) {
     // Alert players that they are starting the round
     window.alert("Welcome to Tobot Gladiators!");
  
@@ -77,4 +76,9 @@ var fight = function() {
 };
 
 // run the fight function
-//fight();
+//fight()
+
+for (var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+
+}
