904127436a24d6b358b508fa2ec8905c15be286a
jiajie chen <ga-kit@hotmail.com>
2021-05-22 18:01:30 -0700

    Fixing no input with the player-name prompt

--- Unsolved/game.js
+++ Unsolved/game.js
@@ -80,6 +80,7 @@ for (var i = 0; i < enemyInfo.length; i++) {
     // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
     window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
 
+
     var pickedEnemyObj = enemyInfo[i];
 
     // reset enemyHealth before starting new fight
@@ -160,8 +161,24 @@ var shop = function() {
     }
 }
 
+// function to set name
+var getPlayerName = function() {
+    var name = "";
+  
+  // ADD LOOP HERE WITH PROMPT AND CONDITION
+  
+    console.log("Your robot's name is " + name);
+    return name;
+  };
+
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+  
+
 var playerInfo = {
     name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
